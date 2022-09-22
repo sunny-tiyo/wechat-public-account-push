9d12d2124166b9eec93ddcb48c26ae16ab5e6694
sunny-tiyo <114122366+sunny-tiyo@users.noreply.github.com>
2022-09-22 10:50:21 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -78,8 +78,8 @@ export const USER_CONFIG = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
-        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        {"type": "*生日", "name": "最最最亲爱的老婆", "year": "2003", "date": "08-18"},
+        {"type": "节日", "name": "纪念日", "year": "2022", "date": "02-19"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
         {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
@@ -87,21 +87,21 @@ export const USER_CONFIG = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "love_day", date: "2022-09-08"},
-        // 结婚纪念日
+        {"keyword": "love_day", date: "2022-2-19"},
+        // 恋爱纪念日
         {"keyword": "marry_day", date: "2022-09-09"},
         // 退伍日
         {"keyword": "ex_day", date: "2022-09-10"},
       ]
     },
     {
-      name: "老婆1",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
-      horoscopeDateType: '',
+      name: "最最最亲爱的老婆",
+      id: "oJ3md6bKSBdAdF4zG4jGI8vlgglY",
+      useTemplateId: "hohgR3BkTyO6vboiB74Y7Qd9aw5I4Zw8pGGmknEGTN4",
+      province: "河南",
+      city: "驻马店",
+      horoscopeDate: '08-18',
+      horoscopeDateType: '今日',
       openUrl: "https://wangxinleo.cn",
       festivals: [],
       customizedDateList: []
@@ -160,9 +160,9 @@ export const USER_CONFIG = {
   /** 天气相关 */
   
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "河南",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "驻马店",
   
   /** 重要节日相关 */
   
@@ -185,8 +185,8 @@ export const USER_CONFIG = {
    */
   FESTIVALS: [
     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-    {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09", isShowAge: true},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+    {"type": "*生日", "name": "最最最亲爱的老婆", "year": "2003", "date": "08-18", isShowAge: true},
+    {"type": "节日", "name": "恋爱纪念日", "year": "2022", "date": "02-19"},
     // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
     {"type": "生日", "name": "李四", "year": "1996", "date": "09-31", isShowAge: true},
     {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
@@ -199,7 +199,7 @@ export const USER_CONFIG = {
    * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
    * 如果为0, 则默认展示全部
    */
-  FESTIVALS_LIMIT: 4,
+  FESTIVALS_LIMIT:1,
   
   /** 日期相关 */
   
@@ -209,8 +209,8 @@ export const USER_CONFIG = {
    * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2022-09-08"},
-    // 结婚纪念日
+    {"keyword": "love_day", date: "2022-02-19"},
+    // 恋爱纪念日
     {"keyword": "marry_day", date: "2022-09-09"},
     // 退伍日
     {"keyword": "ex_day", date: "2022-09-10"},
